8ad1752a233d79ea14ab4c0c66ce2c64e9f8535d
Harathi9 <98879412+Harathi9@users.noreply.github.com>
2022-02-21 18:34:25 +0530

    #include<stdio.h> int main() {     int num,even,odd;     scanf("%d",&num);     if(num%2==0)     {         printf("Even");     }     else     {         printf("Odd");     }     return 0; }

---
+++ Check_if_number_is_even_or_odd.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int num,even,odd;
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+        printf("Odd");
+    }
+    return 0;
+}
